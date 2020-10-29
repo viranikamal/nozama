245ad3b1e548dc411d8248a820f422bac3db6857
virani kamal <viranikamal6666@gmail.com>
2020-10-29 19:13:56 -0400

    my changes

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 // DOM elements
 const hum_menu = document.querySelector('#humberger');
 const display_menu = document.getElementById('display_side_menu');
+const location_button = document.querySelector('#lct');
+const location_box = document.getElementById('location_box');
+const remove = document.querySelector('#remove');
 // Events...
 hum_menu.addEventListener('click' , funOne);
-
+location_button.addEventListener('click' , funTwo);
+remove.addEventListener('click' , funThree);
 // function..
 function funOne(e){
   e.preventDefault();
@@ -17,7 +21,16 @@ function funOne(e){
 
    },4000);
 
-
-  
 }
 
+// funTwo function...
+function funTwo(e){
+  e.preventDefault();
+  location_box.style.display = "block";
+}
+
+// funThree function..
+function funThree(e){
+  e.preventDefault();
+ location_box.style.display = "none";
+}
